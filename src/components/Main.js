d2ae4a2ecc1992dad22b4e5c4f5ff95645db0f21
Gustavo Garcia <gustavogarcia42@yahoo.com>
2022-01-23 20:59:32 -0600

    Starting over

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import About from "./pages/About";
+
+export default function Main() {
+  return <div className="container text-center"></div>;
+}
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  <div className="container text-center">
+    <h1 className="display-1">About Me</h1>
+    <p>
+      Aspiring web develpoer. Currently enrolled in the SMU Coding Bootcamp. I
+      love playing video games and watching sports, mostly soccer. If you have
+      any questions you can reach at my socials or LinkedIn.
+    </p>
+  </div>;
+}
